5dc6728bfd93766fc6e283d5cbf380f3f91e4363
nicholaschan004 <nicholaschan004@gmail.com>
2025-10-07 23:34:07 -0700

    remind me to add diagram

--- add-two-numbers.py
+++ add-two-numbers.py
@@ -2,12 +2,12 @@
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        cur = dummy 
+        dummy = ListNode() # fake starting node
+        cur = dummy # pointer 
 
         carry = 0 
-        while l1 or l2 or carry: 
-            v1 = l1.val if l1 else 0 
+        while l1 or l2 or carry: # runs as long as there's something left to add 
+            v1 = l1.val if l1 else 0 # go thourgh linked list 
             v2 = l2.val if l2 else 0 
 
             val = v1 + v2 + carry 
@@ -16,9 +16,14 @@ class Solution:
             cur.next = ListNode(val)
 
             cur = cur.next 
-            l1 = l1.next if l1 else None 
+            l1 = l1.next if l1 else None # checking if l1 exists 
             l2 = l2.next if l2 else None 
 
         return dummy.next 
 
-        
+    """   
+    example: 
+    val = 4 + 6 + 0 = 10
+    carry = 10 // 10 = 1
+    val = 10 % 10 = 0
+    """
